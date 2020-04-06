d22944158a3edbf2169899d80b86dfbe6b19a9fb
samnied <43746162+samnied@users.noreply.github.com>
2020-04-06 16:18:26 +0200

    Updates.

--- neopixel.ts
+++ neopixel.ts
@@ -253,8 +253,8 @@ namespace neopixel {
         }
         //% blockId="neopixel_show_text"
         //% group=matrix weight=90
-        //% block="%strip show Text $str Line: $line $color"
-        //% line.min = 0 line.max = 1
+        //% block="%strip show Text $str $color" Line: $line
+        //% line.min=0 line.max=1
         //% color.shadow="brightColorNumberPicker"
         showText(str: string, color: number, line: number = 0) {
             this.clear()
